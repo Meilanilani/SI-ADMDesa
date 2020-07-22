37dac697db2c5baa29c078931ab36293a5bf14e5
Meilanilani <meilanianggraeni888@gmail.com>
2020-07-22 08:33:15 +0700

    controller-ktp

--- app/Http/Controllers/KTPSementaraController.php
+++ app/Http/Controllers/KTPSementaraController.php
@@ -147,10 +147,13 @@ class KTPSementaraController extends Controller
         'persuratan.tgl_pembuatan','persuratan.tgl_masa_berlaku','persuratan.status_surat', 'detail_ktp.nik_yg_bersangkutan' )
         ->where('persuratan.id_persuratan',$id_persuratan)
         ->first();
+
+        
         
         $data_warga = DB::table('warga')
         ->where('no_nik', $ktp->nik_yg_bersangkutan)
         ->first();
+       
        
         $ktp = DB::table('persuratan')->where('id_persuratan', $id_persuratan)->first();
         return view('suket-ktp-sementara.edit', compact('ktp', 'data_warga'));
